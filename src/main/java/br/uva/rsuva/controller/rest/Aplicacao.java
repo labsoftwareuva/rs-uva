c50c8a9790ea30ab78007e8a61bfdb607b2d52b4
luisfelipeoliveira <luisfelipeoliveira@192.168.1.122>
2017-05-25 15:06:21 -0300

    teste

--- src/main/java/br/uva/rsuva/controller/rest/Aplicacao.java
+++ src/main/java/br/uva/rsuva/controller/rest/Aplicacao.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+//teste
 @ApplicationPath("/rest")
 public class Aplicacao extends Application {
           @Override
