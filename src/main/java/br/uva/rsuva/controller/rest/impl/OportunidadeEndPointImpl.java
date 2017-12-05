b1e32e062661771723fb4f9d1ff87cf756fc33bf
TiagoCaborsa <tiago.gnr1985@gmail.com>
2017-12-05 21:25:48 -0200

    oportunidade

--- src/main/java/br/uva/rsuva/controller/rest/impl/OportunidadeEndPointImpl.java
+++ src/main/java/br/uva/rsuva/controller/rest/impl/OportunidadeEndPointImpl.java
@@ -3,8 +3,10 @@ package br.uva.rsuva.controller.rest.impl;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,66 +18,51 @@ import br.uva.rsuva.model.dao.impl.OportunidadeDAOImpl;
 import br.uva.rsuva.model.entity.Oportunidade;
 
 @Path("/oportunidade")
-public class OportunidadeEndPointImpl implements OportunidadeEndPoint{
-	
+public class OportunidadeEndPointImpl implements OportunidadeEndPoint {
+
 	private OportunidadeDAO oportunidadeDAO;
-	
-	@Override
-	@GET
-	@Path("obter/{id}")
-	@Produces("application/json")
-	public Oportunidade getOportunidade(@PathParam("id") int id) {
-		oportunidadeDAO = new OportunidadeDAOImpl();
-		
-		Oportunidade oportunidade = oportunidadeDAO.getOportunidade(id);
-				
-		return oportunidade;
-	}
 
 	@Override
 	@GET
-	@Path("/listarTodos")
+	@Path("/list")
 	@Produces("application/json")
 	public List<Oportunidade> getOportunidades() {
-		
 		oportunidadeDAO = new OportunidadeDAOImpl();
 		return oportunidadeDAO.listar();
-	
+
 	}
 
 	@Override
 	@POST
-	@Path("/cadastrar")
+	@Path("/new")
 	@Produces("application/json")
-	@Consumes({MediaType.APPLICATION_JSON})
+	@Consumes({ MediaType.APPLICATION_JSON })
 	public int incluirOportunidade(Oportunidade oportunidade) {
-		
 		oportunidadeDAO = new OportunidadeDAOImpl();
 		return oportunidadeDAO.incluir(oportunidade);
-		
+
 	}
 
 	@Override
-	@POST
-	@Path("alterar")
+	@DELETE
+	@Path("delete/{id}")
 	@Produces("application/json")
-	@Consumes({MediaType.APPLICATION_JSON})
-	public boolean alterarOportunidade(Oportunidade oportunidade) {
-		
-		oportunidadeDAO = new OportunidadeDAOImpl();
-		return oportunidadeDAO.alterar(oportunidade);
-	
-	}
-
-	@Override
-	@POST
-	@Path("excluir")
-	@Produces("application/json")
-	@Consumes({MediaType.APPLICATION_JSON})
-	public boolean removerOportunidade(Oportunidade oportunidade) {
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public boolean removerOportunidade(@PathParam("id") int id) {
+		Oportunidade oportunidade = new Oportunidade();
+		oportunidade.setId(id);
 		oportunidadeDAO = new OportunidadeDAOImpl();
 		return oportunidadeDAO.excluir(oportunidade);
 	}
-
 	
+	@Override
+	@PUT
+	@Path("edit")
+	@Produces("application/json")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public boolean alterarOportunidade(Oportunidade oportunidade) {
+		oportunidadeDAO = new OportunidadeDAOImpl();
+		return oportunidadeDAO.alterar(oportunidade);
+	}
+
 }
